13f3de8c3a8d2d51047fe1f423c2cd118875abc4
Raluca158 <raluca.doodle@yahoo.com>
2021-07-11 21:24:49 +0200

    Added forecast API (not finished)

--- src/index.js
+++ src/index.js
@@ -15,11 +15,35 @@ let minutes = time.getMinutes();
 currentDate.innerHTML = ` ${day}, ${hour}:${minutes}`;
 
 
-let cityInput = document.querySelector("#city-input");
-let city = cityInput.value;
-let apiKey = "fa8e748cbde4ed02a809ebf9c178a37b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemp);
+
+
+
+function displayForecast (response) {
+  console.log(response.data.daily);
+  let forecastElement= document.querySelector("#forecast");
+  let days = ["Thursday", "Friday", "Saturday", " Sunday", "Monday"];
+  let forecastHTML = `<div class = "col-5">`;
+
+  days.forEach(function (day) {
+  forecastHTML = forecastHTML + 
+  `
+  <div class="card text-center" style="width: 15rem;">
+  <div class="card-body">
+    <h5 class="card-title">${day}</h5>
+    <p class="card-text">16°C ⛅</p>
+  </div>
+  </div>
+  `;
+ });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+}
+function getForecast(coordinates) {
+  let apiKey = "fa8e748cbde4ed02a809ebf9c178a37b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemp(response) {
   let cityName = document.querySelector("#current-city");
@@ -53,23 +77,27 @@ function displayTemp(response) {
   let maxTemp = Math.round(response.data.main.temp_max);
   let displayMaxTemp = document.querySelector("#max-temp");
   displayMaxTemp.innerHTML = `Max. temp: ${maxTemp}°C`;
+
+  
   
 }
 
 function search(event) {
-  event.preventDefault(event);
+  event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let currentCity = document.querySelector("#current-city");
   let city = cityInput.value;
   currentCity.innerHTML = city;
-
+  
   apiKey = "fa8e748cbde4ed02a809ebf9c178a37b";
   apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemp);
 }
 
+
 let form = document.querySelector("#city-search");
 form.addEventListener("submit", search);
+
 
 function getCurrentLocation(event) {
   event.preventDefault();
@@ -89,18 +117,21 @@ function displayFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemp);}
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
 
 function displayCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemp);
-
-  
 }
+
+
 let celsiusTemp = null;
 let fahrenheitLink = document.querySelector("#fahrenheit-unit");
 fahrenheitLink.addEventListener("click", displayFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-unit");
 celsiusLink.addEventListener("click", displayCelsius);
+
+
